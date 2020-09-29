5e4a1e75725a61357b6e20ca0b0273cc46a013a3
Francesca Castleton <csinocruz@gmail.com>
2020-09-28 18:07:12 -0700

    creating repo for extra practice

---
+++ practice-00.cpp
@@ -0,0 +1,18 @@
+/**
+ *  @author Francesca Sinocruz
+ *  @date 09/28/2020
+ *  @file practice-00.cpp
+ */
+#include <iostream>
+#include <string>
+using namespace std;
+
+string name = "Cesca";  
+
+/**
+ * One lonely greeting to myself.
+ */
+void main()
+{
+    cout << "Hello, my name is " << name << endl;
+}
